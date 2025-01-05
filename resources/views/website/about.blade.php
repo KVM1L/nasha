e1cd1e487ca3220328ce1497babe09d248ab49d5
KVM1L <kamil.abbaszade@gmail.com>
2025-01-05 14:08:56 +0400

    ml-5 carousel

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -104,7 +104,7 @@
                 <h5 class="text-center my-5">{{ __('Special thanks') }}</h5>
                 <div class="owl-carousel owl-theme pb-5">
                     @foreach ($sponsors as $sponsor)
-                        <div class="item ml-3">
+                        <div class="item ml-5">
                             <a href="{{ $sponsor->url ?? '#' }}" target="_blank" rel="noopener">
                                 <img src="{{ Storage::url($sponsor->logo) }}" alt="{{ $sponsor->name }}">
                             </a>
@@ -121,7 +121,7 @@
                 <h5 class="text-center my-5">{{ __('They trust us') }}</h5>
                 <div class="owl-carousel owl-theme mb-5">
                     @foreach ($clients as $client)
-                        <div class="item">
+                        <div class="item ml-5">
                             <a href="{{ $client->url ?? '#' }}" target="_blank" rel="noopener">
                                 <img src="{{ Storage::url($client->logo) }}" alt="{{ $client->name }}">
                             </a>
